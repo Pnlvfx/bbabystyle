25e32f0791b36ce28f8ab6e04714ccbe8f93a2f9
simone <simonegauli@gmail.com>
2023-02-27 19:56:29 +0100

    use auto Ads

--- components/post/Feed.tsx
+++ components/post/Feed.tsx
@@ -1,21 +1,20 @@
-"use client";
-import { useState } from "react";
-import InfiniteScroll from "react-infinite-scroll-component";
-import postapis from "../API/postapis/postapis";
-import Adsense from "../google/Adsense";
-import Post from "./Post";
-import PostModal from "./PostModal";
+'use client'
+import { useState } from 'react'
+import InfiniteScroll from 'react-infinite-scroll-component'
+import postapis from '../API/postapis/postapis'
+import Post from './Post'
+import PostModal from './PostModal'
 
 type FeedProps = {
-  posts: PostProps[];
-  community?: CommunityProps;
-  author?: string;
-};
+  posts: PostProps[]
+  community?: CommunityProps
+  author?: string
+}
 
 const Feed = ({ posts: ssrPost, community, author }: FeedProps) => {
-  const [posts, setPosts] = useState(ssrPost);
-  const [hasMore, setHasMore] = useState(true);
-  const [postForModal, setPostForModal] = useState<PostProps>();
+  const [posts, setPosts] = useState(ssrPost)
+  const [hasMore, setHasMore] = useState(true)
+  const [postForModal, setPostForModal] = useState<PostProps>()
 
   const getMorePosts = async () => {
     try {
@@ -23,31 +22,31 @@ const Feed = ({ posts: ssrPost, community, author }: FeedProps) => {
         community: community?.name,
         author,
         limit: 10,
-      });
+      })
       if (newPosts.length < 10) {
-        setHasMore(false);
+        setHasMore(false)
       }
-      setPosts([...posts, ...newPosts]);
+      setPosts([...posts, ...newPosts])
     } catch (err) {}
-  };
+  }
   return (
     <>
       {postForModal && (
         <PostModal
           post={postForModal}
           onClickOut={() => {
-            setPostForModal(undefined);
+            setPostForModal(undefined)
           }}
         />
       )}
       <div>
         <InfiniteScroll dataLength={posts?.length || 1} next={getMorePosts} hasMore={hasMore} loader={<div />} endMessage={<></>}>
           {posts?.length >= 1 ? (
-            posts.map((post, index) => {
-              if (index === 3 && process.env.NODE_ENV === "production") {
-                return <Adsense key={index} />;
-              }
-              return <Post key={post._id} post={post} isListing={true} setPostForModal={setPostForModal} />;
+            posts.map((post) => {
+              // if (index === 3 && process.env.NODE_ENV === "production") {
+              //   return <Adsense key={index} />;
+              // }
+              return <Post key={post._id} post={post} isListing={true} setPostForModal={setPostForModal} />
             })
           ) : (
             <div></div>
@@ -55,7 +54,7 @@ const Feed = ({ posts: ssrPost, community, author }: FeedProps) => {
         </InfiniteScroll>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Feed;
+export default Feed
