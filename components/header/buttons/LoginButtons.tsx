8708d348a7365f3f89aa3a8d7202f08a9c7708dd
simone <simonegauli@gmail.com>
2023-05-03 16:42:02 +0200

    new

--- components/header/buttons/LoginButtons.tsx
+++ components/header/buttons/LoginButtons.tsx
@@ -2,13 +2,14 @@
 
 import Link from 'next/link'
 import { useModals } from '../../auth/modal/ModalsProvider'
+import { clientUrl } from '../../../config/config'
 
 const LoginButtons = () => {
   const modals = useModals()
   return (
     <>
       <Link
-        href={'/account/register'}
+        href={`${clientUrl}/account/register`}
         role={'button'}
         tabIndex={0}
         className="relative ml-1 flex min-h-[32px] w-auto min-w-[32px] items-center justify-center rounded-full border border-[#d7dadc] bg-transparent fill-white px-4 py-1 text-center text-[14px] font-bold leading-[17px] text-white lg:ml-4 lg:w-[120px]"
@@ -20,7 +21,7 @@ const LoginButtons = () => {
         <p>Sign Up</p>
       </Link>
       <Link
-        href={'account/login'}
+        href={`${clientUrl}/account/login`}
         role={'button'}
         tabIndex={0}
         className="relative ml-1 flex min-h-[32px] w-auto min-w-[32px] items-center justify-center rounded-full border-none bg-white px-4 py-1 text-center text-[14px] font-bold leading-[17px] text-black lg:ml-4 lg:w-[120px]"
